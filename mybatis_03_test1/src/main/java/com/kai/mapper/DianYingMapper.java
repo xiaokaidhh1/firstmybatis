443e25b0322700fb8c91dba6356ff035d51ac9cb
xiaokai <1152753611@qq.com>
2022-03-28 16:44:18 +0800

    新增mybatis实验一

---
+++ mybatis_03_test1/src/main/java/com/kai/mapper/DianYingMapper.java
@@ -0,0 +1,27 @@
+package com.kai.mapper;
+
+import com.kai.pojo.DianYing;
+import com.kai.pojo.TiaoJian;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+public interface DianYingMapper {
+    List<DianYing> selectAll();
+    List<DianYing> selectAllByLeixingId();
+
+//    @Results(id = "rs" ,value= {
+//            @Result(property = "leixingId", column = "leixing_id")
+//    })
+//    @ResultMap("rs")
+    @Select("select * from dian_ying where id=#{id}")
+    DianYing selectByzhuJie(Integer id);
+
+
+
+    //2.Mapper接口中（dao）使用注解 调用provider类的方法返回的动态sql
+    @SelectProvider(type = DianYingDynamicSql.class,method = "chaXunSql")
+    List<DianYing> chaDianYingHeiHei(TiaoJian tj);
+
+}
+
---
+++ mybatis_03_test1/src/main/java/com/kai/pojo/TiaoJian.java
@@ -0,0 +1,40 @@
+package com.kai.pojo;
+
+public class TiaoJian {
+    private String ming;
+    private String YanYuan;
+    private String LeixingId;
+    private String DiJia;
+
+    public String getMing() {
+        return ming;
+    }
+
+    public void setMing(String ming) {
+        this.ming = ming;
+    }
+
+    public String getYanYuan() {
+        return YanYuan;
+    }
+
+    public void setYanYuan(String yanYuan) {
+        YanYuan = yanYuan;
+    }
+
+    public String getLeixingId() {
+        return LeixingId;
+    }
+
+    public void setLeixingId(String leixingId) {
+        LeixingId = leixingId;
+    }
+
+    public String getDiJia() {
+        return DiJia;
+    }
+
+    public void setDiJia(String diJia) {
+        DiJia = diJia;
+    }
+}
